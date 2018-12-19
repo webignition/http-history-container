e6bb1fb6a4b81fad94e3cc776c1169bf033c0f66
Jon Cram <jon@webignition.net>
2018-12-19 19:19:40 +0000

    Remove ContainerInterface

--- src/Container.php
+++ src/Container.php
@@ -6,7 +6,7 @@ use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\UriInterface;
 
-class Container implements ContainerInterface, \ArrayAccess, \Iterator, \Countable
+class Container implements \ArrayAccess, \Iterator, \Countable
 {
     const KEY_REQUEST = 'request';
     const KEY_RESPONSE = 'response';
