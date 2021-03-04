30fce91fee0d8fbd47b89ad8e35519aa6eaf4a0f
Jon Cram <webignition@gmail.com>
2021-03-04 10:18:30 +0000

    Create ResponseCollectionInterface

---
+++ src/Collection/ResponseCollectionInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\HttpHistoryContainer\Collection;
+
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * @extends \IteratorAggregate<int, ResponseInterface>
+ */
+interface ResponseCollectionInterface extends \Countable, \IteratorAggregate
+{
+    /**
+     * @return \Iterator<int, ResponseInterface>
+     */
+    public function getIterator(): \Iterator;
+    public function getLast(): ?ResponseInterface;
+}
