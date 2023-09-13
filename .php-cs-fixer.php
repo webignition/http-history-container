6547f0a98732af44e614c0cfa0860515a3ef13b5
Jon Cram <webignition@gmail.com>
2023-09-13 11:15:18 +0100

    Use php-cs-fixer to enforce strict types

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -15,4 +15,5 @@ return $config->setRules([
     'php_unit_internal_class' => false,
     'php_unit_test_class_requires_covers' => false,
     'single_line_empty_body' => false,
+    'declare_strict_types' => true,
 ])->setFinder($finder);
