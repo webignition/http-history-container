fc73c8160a3d4a8e5749f94a8a2b6dbc2379e6b9
Jon Cram <webignition@gmail.com>
2023-09-13 11:03:40 +0100

    Use php-cs-fixer 3.26.1 (#117)

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -14,4 +14,5 @@ return $config->setRules([
     'trailing_comma_in_multiline' => false,
     'php_unit_internal_class' => false,
     'php_unit_test_class_requires_covers' => false,
+    'single_line_empty_body' => false,
 ])->setFinder($finder);
